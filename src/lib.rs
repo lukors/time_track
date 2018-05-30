a6ec88acf6795326eb1f8a6199e7a8a7a63c575b
Lukas Orsvärn <lukas@orsvarn.com>
2018-05-30 22:05:09 +0200

    some poking around, think I figured out how to store and use the time

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,16 @@
+#[macro_use]
+extern crate serde_derive;
+
+extern crate serde;
+extern crate serde_json;
+
 extern crate chrono;
 
 use chrono::prelude::*;
+use std::{
+    path::Path,
+    io,
+};
 
 #[cfg(test)]
 mod tests {
@@ -9,7 +19,7 @@ mod tests {
     #[test]
     fn add_event() {
         let event = Event{
-            time: Utc::now(),
+            time: UnixTime::from_timestamp(Utc::now().timestamp()),
             description: "This is a description".to_string(),
             tag_ids: vec!(),
         };
@@ -20,22 +30,62 @@ mod tests {
         assert_eq!(event, event_db.events[0]);
     }
 
-    // #[test]
-    // fn write_db() {
-    //     let event_db: EventDB = EventDB{
-    //         events: vec!(),
-    //     }
-    //     let file_name = "write_test";
+    #[test]
+    fn write_db() {
+        let file_name = "write_test.txt";
+        let event_db = EventDB::new();
+        // TODO: Add some things to the DB that can be written.
         
-    //     super::write_db(file_name, db);
+        //assert!(super::write_db(event_db, Path::new(file_name)).is_ok());
+    }
 
+    // TODO: Test for reading DB
+    // #[test]
+    // fn read_db() {
+    //     let file_name = "write_test.txt";
+    //     let event_db = EventDB::new();
+    //     let target_contents = "whatever should be in the file";
+        
+    //     super::write_db(event_db, file_name);
+        
+    //     // TODO: Read the file into a variable to be compared in the assert_eq.
+    //     // let written_contents = file.read(file_name);
 
+    //     assert_eq!(target_contents, written_contents);
+    // }
+
+    // TODO: Test for reading and writing DB:
+    // #[test]
+    // fn write_read_db() {
+    //     let file_name = "write_test.txt";
+    //     let event_db = EventDB::new();
+    //     let target_contents = "whatever should be in the file";
+        
+    //     super::write_db(event_db, file_name);
+        
+    //     // TODO: Read the file into a variable to be compared in the assert_eq.
+    //     // let written_contents = file.read(file_name);
+
+    //     assert_eq!(target_contents, written_contents);
     // }
 }
 
-#[derive(Debug, Clone, PartialEq)]
+#[derive(Debug, Clone, PartialEq, Serialize, Deserialize)]
+struct UnixTime {
+    time: i64,
+}
+
+impl UnixTime {
+    fn from_timestamp(secs: i64) -> UnixTime {
+        UnixTime{
+            time: secs,
+        }
+    }
+}
+
+#[derive(Debug, Clone, PartialEq, Serialize, Deserialize)]
 struct Event {
-    time: DateTime<Utc>,
+    time: UnixTime,
     description: String,
     tag_ids: Vec<usize>,
 }
@@ -52,3 +102,7 @@ impl EventDB {
         }
     }
 }
+
+// fn write_db(event_db: EventDB, path: &Path) -> io::Result<()> {
+
+// }
