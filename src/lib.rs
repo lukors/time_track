21d3c1368842a50756616b136a266199c6a25d78
Lukas Orsvärn <lukas@orsvarn.com>
2018-06-03 17:41:32 +0200

    Fixed json writing function so it writes correctly

--- src/lib.rs
+++ src/lib.rs
@@ -13,76 +13,13 @@ use std::{
     io::{self, prelude::*},
     path::Path};
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn write_db() {
-        let file_name = "test_files/write_test.json";
-        let mut event_db = EventDB::new();
-        let time_now = Utc::now().timestamp();
-        event_db.events.insert(
-            time_now,
-            Event {
-            description: "write one, should be over-written due to exact same time".to_string(),
-            tag_ids: vec![2, 1, 4],
-        });
-
-        assert!(super::write_db(&event_db, Path::new(file_name)).is_ok());
-    }
-
-    // TODO: Test for reading DB
-    #[test]
-    fn read_db() {
-        let file_name = "test_files/read_test.json";
-        let mut event_db = EventDB::new();
-        let time_now = Utc::now().timestamp();
-        event_db.events.insert(
-            time_now,
-            Event {
-            description: "write one, should be over-written due to exact same time".to_string(),
-            tag_ids: vec![2, 1, 4],
-        });
-
-        assert!(super::write_db(&event_db, Path::new(file_name)).is_ok());
-    }
-    fn read_db() {
-        let file_name = "write_test.json";
-        let event_db = EventDB::new();
-        let target_contents = "whatever should be in the file";
-
-        super::write_db(event_db, file_name);
-
-        // TODO: Read the file into a variable to be compared in the assert_eq.
-        // let written_contents = file.read(file_name);
-
-        assert_eq!(target_contents, written_contents);
-    }
-
-    // TODO: Test for reading and writing DB:
-    // #[test]
-    // fn write_read_db() {
-    //     let file_name = "write_test.json";
-    //     let event_db = EventDB::new();
-    //     let target_contents = "whatever should be in the file";
-
-    //     super::write_db(event_db, file_name);
-
-    //     // TODO: Read the file into a variable to be compared in the assert_eq.
-    //     // let written_contents = file.read(file_name);
-
-    //     assert_eq!(target_contents, written_contents);
-    // }
-}
-
 #[derive(Debug, Clone, PartialEq, Serialize, Deserialize)]
 struct Event {
     description: String,
     tag_ids: Vec<usize>,
 }
 
-#[derive(Debug)]
+#[derive(Debug, Serialize)]
 struct EventDB {
     events: BTreeMap<i64, Event>,
 }
@@ -95,9 +32,61 @@ impl EventDB {
 
 fn write_db(event_db: &EventDB, path: &Path) -> io::Result<()> {
     let file = File::create(path)?;
-    for (time, event) in &event_db.events {
-        let to_write = (time, &event.description, &event.tag_ids);
-        serde_json::to_writer_pretty(&file, &to_write)?;
-    }
+    // for (time, event) in &event_db.events {
+    //     let to_write = (time, &event.description, &event.tag_ids);
+    // }
+    serde_json::to_writer_pretty(&file, &event_db)?;
     Ok(())
+}
+
+// fn read_db(path: &Path) -> io::Result<EventDB> {
+//     serde_json::
+// }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn write_read_db() {
+        let file_name = "test_files/read_write_test.json";
+        let mut event_db = EventDB::new();
+        
+        let time_now = Utc::now().timestamp();
+        let description = "This event should not exist".to_string();
+        let tag_ids = vec![2, 1, 4];
+        
+        event_db.events.insert(
+            time_now,
+            Event {
+            description,
+            tag_ids,
+        });
+
+        let description = "This is a description".to_string();
+        let tag_ids = vec![3, 1, 1];
+        
+        event_db.events.insert(
+            time_now + 1,
+            Event {
+            description,
+            tag_ids,
+        });
+
+        let description = "This event should exist".to_string();
+        let tag_ids = vec![];
+        
+        event_db.events.insert(
+            time_now,
+            Event {
+            description,
+            tag_ids,
+        });
+
+        assert!(super::write_db(&event_db, Path::new(file_name)).is_ok());
+
+        
+        // let event_db_read = super::read_db(file_name);
+        // assert_eq!(event_db, event_db_read);
+    }
 }
