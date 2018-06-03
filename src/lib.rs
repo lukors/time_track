62189fdf4f919a5f668109be5b5ed00e48a32457
Lukas Orsvärn <lukas@orsvarn.com>
2018-06-03 17:03:28 +0200

    Changed file endings to .json

--- src/lib.rs
+++ src/lib.rs
@@ -19,7 +19,7 @@ mod tests {
 
     #[test]
     fn write_db() {
-        let file_name = "test_files/write_test.txt";
+        let file_name = "test_files/write_test.json";
         let mut event_db = EventDB::new();
         let time_now = Utc::now().timestamp();
         event_db.events.insert(
@@ -33,24 +33,37 @@ mod tests {
     }
 
     // TODO: Test for reading DB
-    // #[test]
-    // fn read_db() {
-    //     let file_name = "write_test.txt";
-    //     let event_db = EventDB::new();
-    //     let target_contents = "whatever should be in the file";
+    #[test]
+    fn read_db() {
+        let file_name = "test_files/read_test.json";
+        let mut event_db = EventDB::new();
+        let time_now = Utc::now().timestamp();
+        event_db.events.insert(
+            time_now,
+            Event {
+            description: "write one, should be over-written due to exact same time".to_string(),
+            tag_ids: vec![2, 1, 4],
+        });
 
-    //     super::write_db(event_db, file_name);
+        assert!(super::write_db(&event_db, Path::new(file_name)).is_ok());
+    }
+    fn read_db() {
+        let file_name = "write_test.json";
+        let event_db = EventDB::new();
+        let target_contents = "whatever should be in the file";
 
-    //     // TODO: Read the file into a variable to be compared in the assert_eq.
-    //     // let written_contents = file.read(file_name);
+        super::write_db(event_db, file_name);
 
-    //     assert_eq!(target_contents, written_contents);
-    // }
+        // TODO: Read the file into a variable to be compared in the assert_eq.
+        // let written_contents = file.read(file_name);
+
+        assert_eq!(target_contents, written_contents);
+    }
 
     // TODO: Test for reading and writing DB:
     // #[test]
     // fn write_read_db() {
-    //     let file_name = "write_test.txt";
+    //     let file_name = "write_test.json";
     //     let event_db = EventDB::new();
     //     let target_contents = "whatever should be in the file";
 
