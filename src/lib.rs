ac260c1cb8899a55a046012039eb24e8a8e2d261
Lukas Orsvärn <lukas@orsvarn.com>
2018-06-03 17:00:06 +0200

    Writing database works, decided on datastructure to use

--- src/lib.rs
+++ src/lib.rs
@@ -7,49 +7,25 @@ extern crate serde_json;
 extern crate chrono;
 
 use chrono::prelude::*;
-use std::{fs::File,
-          io::{self, prelude::*},
-          path::Path};
+use std::{
+    collections::BTreeMap,
+    fs::File,
+    io::{self, prelude::*},
+    path::Path};
 
 #[cfg(test)]
 mod tests {
     use super::*;
 
     #[test]
-    fn add_event() {
-        let event = Event {
-            time: UnixTime::from_timestamp(Utc::now().timestamp()),
-            description: "This is a description".to_string(),
-            tag_ids: vec![],
-        };
-
-        let mut event_db = EventDB::new();
-        event_db.events.push(event.clone());
-
-        assert_eq!(event, event_db.events[0]);
-    }
-
-    #[test]
     fn write_db() {
-        let file_name = "target/test_files/write_test.txt";
+        let file_name = "test_files/write_test.txt";
         let mut event_db = EventDB::new();
-        // TODO: Add some things to the DB that can be written.
         let time_now = Utc::now().timestamp();
-        event_db.events.push(Event {
-            time: UnixTime { time: time_now },
+        event_db.events.insert(
+            time_now,
+            Event {
             description: "write one, should be over-written due to exact same time".to_string(),
-            tag_ids: vec![2, 1, 4],
-        });
-        event_db.events.push(Event {
-            time: UnixTime { time: time_now },
-            description:
-                "write two, should be visible since it has the exact same time as previous write"
-                    .to_string(),
-            tag_ids: vec![2, 1, 4],
-        });
-        event_db.events.push(Event {
-            time: UnixTime { time: time_now + 1 },
-            description: "one second later".to_string(),
             tag_ids: vec![2, 1, 4],
         });
 
@@ -87,40 +63,28 @@ mod tests {
     // }
 }
 
-#[derive(Debug, Copy, Clone, PartialEq, Serialize, Deserialize)]
-struct UnixTime {
-    time: i64,
-}
-
-impl UnixTime {
-    fn from_timestamp(secs: i64) -> UnixTime {
-        UnixTime { time: secs }
-    }
-}
-
 #[derive(Debug, Clone, PartialEq, Serialize, Deserialize)]
 struct Event {
-    time: UnixTime,
     description: String,
     tag_ids: Vec<usize>,
 }
 
 #[derive(Debug)]
 struct EventDB {
-    events: Vec<Event>,
+    events: BTreeMap<i64, Event>,
 }
 
 impl EventDB {
     fn new() -> EventDB {
-        EventDB { events: vec![] }
+        EventDB { events: BTreeMap::new() }
     }
 }
 
 fn write_db(event_db: &EventDB, path: &Path) -> io::Result<()> {
     let file = File::create(path)?;
-    for event in &event_db.events {
-        serde_json::to_writer_pretty(&file, &event)?;
+    for (time, event) in &event_db.events {
+        let to_write = (time, &event.description, &event.tag_ids);
+        serde_json::to_writer_pretty(&file, &to_write)?;
     }
-    // file.write_all(serialized)?;
     Ok(())
 }
